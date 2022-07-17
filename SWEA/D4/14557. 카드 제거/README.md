63446e8ee0b7dbd4a9850b3667f1a24a427c732f
talk2you <51767056+talk2you@users.noreply.github.com>
2022-07-18 03:34:11 +0900

    [D4] Title: 카드 제거, Time: 406 ms, Memory: 7,784 KB -BaekjoonHub

---
+++ SWEA/D4/14557. 카드 제거/README.md
@@ -0,0 +1,11 @@
+# [D4] 카드 제거 - 14557 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYGt7M5qAbkDFARC) 
+
+### 성능 요약
+
+메모리: 7,784 KB, 시간: 406 ms, 코드길이: 1,452 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/14557. 카드 제거/카드 제거.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_QUE 100003
+
+char list[100003];
+
+int deque[100003];
+int front, end;
+
+void init()
+{
+	front = end = 0;
+}
+
+int isEmpty()
+{
+	if (front == end)
+		return 1;
+	return 0;
+}
+
+int isFull()
+{
+	int temp = front - 1;
+	
+	if (temp < 0)
+		temp += MAX_QUE;
+
+	if (temp == end)
+		return 1;
+	return 0;
+}
+
+int push_front(int idx)
+{
+	if (isFull())
+		return 0;
+
+	front--;
+	if (front < 0)
+		front += MAX_QUE;
+	deque[front] = idx;
+	return 1;
+}
+
+int pop_front(int *idx)
+{
+	if (isEmpty())
+		return 0;
+
+	*idx = deque[front++];
+	front %= MAX_QUE;
+	return 1;
+}
+
+int push_back(int idx)
+{
+	if (isFull())
+		return 0;
+
+	deque[end++] = idx;
+	end %= MAX_QUE;
+	return 1;
+}
+
+void input()
+{
+	int i = 0;
+	
+	scanf(" %s", list);
+	
+}
+
+void solve()
+{
+	int i = 0;
+
+	while (list[i])
+	{
+		if(list[i] == '1')
+			push_back(i);
+		++i;
+	}
+
+	int idx, dummy;
+	while (pop_front(&idx))
+	{
+		list[idx] = 'X';
+
+		if (list[idx + 1] == '0')
+		{
+			push_front(idx + 1);
+			list[idx + 1] = '1';
+		}
+		else if (list[idx + 1] == '1')
+		{
+			pop_front(&dummy);
+			list[idx + 1] = '0';
+		}
+
+		if (list[idx - 1] == '0')
+		{
+			push_front(idx - 1);
+			list[idx - 1] = '1';
+		}
+	}
+
+	for (int i = strlen(list); i >= 0; --i)
+	{
+		if (list[i] == '0')
+		{
+			printf("no\n");
+			return;
+		}
+	}
+
+	printf("yes\n");
+}
+
+int main(void)
+{
+	int T;
+
+	setbuf(stdout, NULL);
+	scanf("%d", &T);
+
+	for (int t = 1; t <= T; t++)
+	{
+		init();
+		input();
+		printf("#%d ", t);
+		solve();
+	}
+}
