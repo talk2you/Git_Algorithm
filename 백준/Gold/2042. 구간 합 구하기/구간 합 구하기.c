3da2144e2e477339673eb30f7215783acf08a546
talk2you <51767056+talk2you@users.noreply.github.com>
2022-07-21 17:08:45 +0900

    [Gold I] Title: 구간 합 구하기, Time: 348 ms, Memory: 40176 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.c
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.c
@@ -4,77 +4,87 @@ int N, M, K;
 long long data[1000003];
 long long tree[4000003];
 
-long long init_segment_tree(int start, int end, int idx)
+void input()
 {
+	scanf("%d %d %d", &N, &M, &K);
+
+	for (int i = 1; i <= N; i++)
+		scanf("%lld", &data[i]);
+}
+
+long long make_tree(int start, int end, int idx)
+{
+	int mid = (start + end) >> 1;
 	if (start == end)
 	{
 		tree[idx] = data[start];
 		return tree[idx];
 	}
 
-	int mid = (start + end) / 2;
-	tree[idx] = init_segment_tree(start, mid, idx * 2) + init_segment_tree(mid + 1, end, idx * 2 + 1);
+	tree[idx] = make_tree(start, mid, idx * 2) + make_tree(mid + 1, end, idx * 2 + 1);
 	return tree[idx];
-}
-
-void input()
-{
-	scanf("%d %d %d", &N, &M, &K);
-
-	for (int i = 1;i <= N;i++)
-		scanf("%lld", &data[i]);
 }
 
 long long interval_sum(int start, int end, int idx, int left, int right)
 {
-	if (left > end || right < start)
-		return 0;
+	int mid = (start + end) >> 1;
 
-	if (left <= start && right >= end)
+	// 구간을 벗어나는 경우
+	if (end < left || right < start)
+		return 0;
+	
+	// 구간안에 있는 경우
+	if (left <= start && end <= right)
 		return tree[idx];
 
-	int mid = (start + end) / 2;
+	// 한쪽씩 걸친 경우
 	return interval_sum(start, mid, idx * 2, left, right) + interval_sum(mid + 1, end, idx * 2 + 1, left, right);
 }
 
-void update(int start, int end, int idx, int what, long long value)
+void update(int start, int end, int idx, int target_idx, long long diff_data)
 {
-	if (what < start || what > end)
+	if (target_idx < start || end < target_idx)
 		return;
 
-	tree[idx] += value;
+	tree[idx] += diff_data;
 
 	if (start >= end)
 		return;
 
-	int mid = (start + end) / 2;
-	update(start, mid, idx * 2, what, value);
-	update(mid + 1, end, idx * 2 + 1, what, value);
+	int mid = (start + end) >> 1;
+	update(start, mid, idx * 2, target_idx, diff_data);
+	update(mid + 1, end, idx * 2 + 1, target_idx, diff_data);
 }
 
 void solve()
 {
-	int a, b;
-	long long c;
-	
-	init_segment_tree(1, N, 1);
+	int cmd, a, b;
+	long long bb;
 
-	for (int i = 0;i < M + K;++i)
+	make_tree(1, N, 1);
+
+	for (int i = 0; i < M + K; i++)
 	{
-		scanf("%d %d %lld", &a, &b, &c);
+		scanf("%d", &cmd);
 
-		if (a == 1)
+		// data[a] -> b로 바꾸기
+		if (cmd == 1)
 		{
-			long long diff = c - data[b];
-			data[b] = c;
-			update(1, N, 1, b, diff);
+			scanf(" %d %lld", &a, &bb);
+			long long diff = bb - data[a];
+			data[a] = bb;
+			update(1, N, 1, a, diff);
 		}
+		// data[a] ~ data[b]의 부분합 출력
 		else
-			printf("%lld\n", interval_sum(1, N, 1, b, (int)c));
+		{
+			scanf(" %d %d", &a, &b);
+			printf("%lld\n", interval_sum(1, N, 1, a, b));
+		}
 	}
 }
 
-int main(void)
+int main()
 {
 	input();
 	solve();
